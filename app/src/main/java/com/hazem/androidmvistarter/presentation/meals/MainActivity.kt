6150ba6225226b6b3970781ff9cd333e90fdf0c5
hazem <hazem.ashraf126@gmail.com>
2022-03-04 13:56:42 +0200

    update ui

--- app/src/main/java/com/hazem/androidmvistarter/presentation/meals/MainActivity.kt
+++ app/src/main/java/com/hazem/androidmvistarter/presentation/meals/MainActivity.kt
@@ -3,18 +3,24 @@ package com.hazem.androidmvistarter.presentation.meals
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberImagePainter
 import com.hazem.androidmvistarter.ui.theme.AndroidMVIStarterTheme
+import com.hazem.androidmvistarter.utils.network.NetworkUtils.BASE_URL
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,8 +34,11 @@ class MainActivity : ComponentActivity() {
                     items(
                         state.meals
                     ){ meal ->
-                        Surface {
-                            Text(text = meal.title.toString())
+                        Card(elevation = 2.dp, modifier = Modifier.fillMaxWidth().padding(8.dp), shape = RoundedCornerShape(size = 16.dp)) {
+                            Row (verticalAlignment = Alignment.CenterVertically){
+                                Image(painter = rememberImagePainter("$BASE_URL${meal.image}"), contentDescription = null, modifier = Modifier.size(128.dp))
+                                Text(text = meal.title.toString(), modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+                            }
                         }
                     }
                 }
