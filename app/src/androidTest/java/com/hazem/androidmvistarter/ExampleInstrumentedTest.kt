e70dae522b2fa210c1252bc4c49ba73b189fe4c5
hazem <hazem.ashraf126@gmail.com>
2022-03-28 16:56:36 +0200

    add bench mark dependancies

--- app/src/androidTest/java/com/hazem/androidmvistarter/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/hazem/androidmvistarter/ExampleInstrumentedTest.kt
@@ -1,5 +1,6 @@
 package com.hazem.androidmvistarter
 
+import androidx.benchmark.junit4.BenchmarkRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -7,6 +8,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Rule
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,6 +17,10 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    @get:Rule
+    val benchmarkRule = BenchmarkRule()
+    
     @Test
     fun useAppContext() {
         // Context of the app under test.
