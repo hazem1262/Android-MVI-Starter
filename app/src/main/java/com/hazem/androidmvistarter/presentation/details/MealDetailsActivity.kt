b6e14c3ee4a1f19b5d3d08113ac6af7c045abb05
hazem <hazem.ashraf126@gmail.com>
2022-04-16 22:48:25 +0200

    navigate to meal details screen

--- app/src/main/java/com/hazem/androidmvistarter/presentation/details/MealDetailsActivity.kt
+++ app/src/main/java/com/hazem/androidmvistarter/presentation/details/MealDetailsActivity.kt
@@ -2,11 +2,23 @@ package com.hazem.androidmvistarter.presentation.details
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MealDetailsActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            Box(Modifier.fillMaxWidth().fillMaxHeight().background(color = Color.Cyan)) {
+
+            }
+        }
     }
 }
--- app/src/main/java/com/hazem/androidmvistarter/presentation/meals/list/views/MealItem.kt
+++ app/src/main/java/com/hazem/androidmvistarter/presentation/meals/list/views/MealItem.kt
@@ -1,5 +1,7 @@
 package com.hazem.androidmvistarter.presentation.meals.list.views
 
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -7,19 +9,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.hazem.androidmvistarter.data.remote.meals.Meal
+import com.hazem.androidmvistarter.presentation.details.MealDetailsActivity
+import com.hazem.androidmvistarter.utils.extensions.getActivity
 import com.hazem.androidmvistarter.utils.network.NetworkUtils
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun MealItem(meal: Meal) {
-    Card(elevation = 2.dp, modifier = Modifier.fillMaxWidth().padding(8.dp), shape = RoundedCornerShape(size = 16.dp)) {
+fun MealItem(meal: Meal, context:Context = LocalContext.current) {
+    Card(
+        elevation = 2.dp,
+        modifier = Modifier.fillMaxWidth().padding(8.dp),
+        shape = RoundedCornerShape(size = 16.dp),
+        onClick = {
+            context.startActivity(Intent(context, MealDetailsActivity::class.java))
+        }
+    ) {
         Row (verticalAlignment = Alignment.CenterVertically){
             Image(painter = rememberImagePainter("${NetworkUtils.BASE_URL}${meal.image}"), contentDescription = null, modifier = Modifier.size(128.dp))
             Text(text = meal.title.toString(), modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
